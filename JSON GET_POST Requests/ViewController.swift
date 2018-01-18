8cb2e6d12a451742b1e87c42bfce73dd7228fa30
Chaudhary Himanshu Raj <chaudharyhimanshuraj@Chaudharys-Mac.local>
2018-01-19 03:30:28 +0530

    Final commit : GET and POST requests can be called by clicking on their respective buttons.

--- JSON GET_POST Requests/ViewController.swift
+++ JSON GET_POST Requests/ViewController.swift
@@ -21,10 +21,50 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getRequestTapped(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                    }
+                }
+            }.resume()
     }
     
     @IBAction func postRequestTapped(_ sender: Any) {
-    }
+        let parameters = ["username": "@Himanshu-Raj", "message": "Getting enough exercise, pushing my luck..!!"]
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        // This line will be setting a value to HTTP header fields, to tell what type of content we will be sending, i.e. here we are sending json so we have set "Content-Type" to "application/json". Try with and without commenting it.
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                    }
+                }
+            }.resume()
+        }
 
 }
 
